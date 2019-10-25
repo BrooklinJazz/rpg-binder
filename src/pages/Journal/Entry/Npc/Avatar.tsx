04bb2aa282c4eafde7cfde1cb6fbcee7663d486d
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-10-24 22:09:19 -0600

    Add temporary avatar uploading

--- src/pages/Journal/Entry/Npc/Avatar.tsx
+++ src/pages/Journal/Entry/Npc/Avatar.tsx
@@ -1,5 +1,5 @@
 import combineClasses from "combine-classes/lib";
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import { faImage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,17 +10,43 @@ import { useNpcEntryContext } from "./context";
 
 const Avatar = () => {
   const { avatar, setAvatar } = useNpcEntryContext();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const triggerInput = () => {
+    if (inputRef.current) {
+      inputRef.current!.click();
+    }
+  };
+  const reader = new FileReader();
+  const [preview, setPreview] = useState();
+  useEffect(() => {
+    if (avatar) {
+      reader.readAsDataURL(avatar);
+      reader.addEventListener("load", () => {
+        setPreview(reader.result);
+      });
+    }
+  }, [avatar]);
+
   return (
     <div className={GridTemplateAreas.NPC_AVATAR}>
       {avatar ? (
-        "Avatar"
+        <img className="AvatarImage" alt="alt" src={preview} />
       ) : (
-        <FontAwesomeIcon
-          className={combineClasses(Theme.hoverable, Theme.onDefault)}
-          size="4x"
-          icon={faImage}
-        />
+        <div role="button" onClick={triggerInput}>
+          <FontAwesomeIcon
+            className={combineClasses(Theme.hoverable, Theme.onDefault)}
+            size="4x"
+            icon={faImage}
+          />
+        </div>
       )}
+      <input
+        onChange={e => e.target.files && setAvatar(e.target.files[0])}
+        style={{ display: "none" }}
+        ref={inputRef}
+        type="file"
+        accept="image/*"
+      />
     </div>
   );
 };
