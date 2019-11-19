1c48c05a7df752929a64ec6e17119de4fdef79cb
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-11-18 22:46:26 -0700

    Configure Entry Name Editing

--- src/pages/Journal/Entry/Name.tsx
+++ src/pages/Journal/Entry/Name.tsx
@@ -1,20 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 
-import { surface2 } from "../../../common/styles";
+import { surface2, primary1, onSurface } from "../../../common/styles";
 import { MarkdownEditor } from "../../../components/MarkdownEditor";
+import { H3 } from "../../../components/StyledTypography";
 import { useEntryState } from "../../../context/journal/entry";
+import useClickoutHandler from "../../../hooks/useClickoutHandler";
+import { ENTRY_HEADING_MAX_LENGTH } from "../../../common/constants";
+import { confirmAlert } from "../../../common/helpers";
 
 const Grid = styled.section`
   grid-area: name;
   background-color: ${surface2};
 `;
 
+const NameInput = styled.input`
+  border: solid 2px ${primary1};
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  background-color: ${surface2};
+  color: ${onSurface};
+  text-align: center;
+  font-family: "Roboto", sans-serif;
+  font-size: 1.75em;
+  font-weight: 200;
+`;
+
+const NameHeader = styled(H3)`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const EditName = ({ stopEditing }: { stopEditing: () => void }) => {
+  const { name, save, setName, revert } = useEntryState();
+  const clickoutRef = useRef(null);
+  const handleSubmit = () => {
+    stopEditing();
+    save();
+  };
+  // TODO refactor key handlers that are copy-pasted.
+  const handleDiscard = () =>
+    confirmAlert({
+      onConfirm: () => {
+        revert();
+        stopEditing();
+      }
+    });
+
+  const aggregateKeyHandlers = (
+    param: React.KeyboardEvent<any>,
+    ...fns: Function[]
+  ) => {
+    fns.forEach(fn => fn(param));
+  };
+
+  const handleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.ctrlKey && e.which === 13) {
+      handleSubmit();
+    }
+  };
+
+  const handleEscape = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Escape") {
+      handleDiscard();
+    }
+  };
+
+  const handleKeyboardShortCuts = (e: React.KeyboardEvent<HTMLInputElement>) =>
+    aggregateKeyHandlers(e, handleEnter, handleEscape);
+
+  useClickoutHandler(clickoutRef, handleSubmit);
+
+  return (
+    <NameInput
+      onKeyUp={handleKeyboardShortCuts}
+      maxLength={ENTRY_HEADING_MAX_LENGTH}
+      autoFocus={true}
+      ref={clickoutRef}
+      onChange={e => setName(e.target.value)}
+      value={name}
+    />
+  );
+};
+
 export const Name = () => {
+  const [editing, setEditing] = useState(false);
+  const stopEditing = () => setEditing(false);
+  const startEditing = () => setEditing(true);
   const { name } = useEntryState();
   return (
     <Grid>
-      <div>{name}</div>
+      {editing ? (
+        <EditName stopEditing={stopEditing} />
+      ) : (
+        <NameHeader onClick={startEditing}>{name}</NameHeader>
+      )}
     </Grid>
   );
 };
