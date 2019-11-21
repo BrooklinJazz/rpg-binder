86a29e11412aa7f42bd69357d600f66f782aaabc
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-11-20 22:39:56 -0700

    Remove unused code

--- src/server/session/session_object.ts
+++ src/server/session/session_object.ts
@@ -1,5 +1,4 @@
-import { IPage, ISection } from "../types";
-import { PageRepo } from "./page_repo";
+import { IPage } from "../types";
 import { SectionRepo } from "../journal/section_repo";
 
 export default class SessionObject {
@@ -14,13 +13,9 @@ export default class SessionObject {
     );
     return SectionRepo.findByIds(sectionIds).then(sections =>
       sections.map(section => {
-        const pages = this.sessionPages.filter(page => {
-          console.log(page.section, section._id, page.section === section._id);
-          return page.section.toString() === section._id.toString();
-        });
         return {
           section,
-          pages
+          pages: this.sessionPages.filter(page => page.section.toString() === section._id.toString());
         };
       })
     );
