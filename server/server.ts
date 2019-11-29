6cb33d1faf553d0d26da5ed67698f601fed1524f
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-11-28 21:52:02 -0700

    change server path

--- server/server.ts
+++ server/server.ts
@@ -13,7 +13,7 @@ import { IAuthData, IContext } from "./types";
 import UserModel from "./user/user_model";
 
 const app = express();
-app.use(express.static(path.join(__dirname, "../build")));
+// app.use(express.static(path.join(__dirname, "../build")));
 
 const server = new ApolloServer({
   typeDefs,
@@ -42,9 +42,9 @@ const server = new ApolloServer({
   playground: false
 });
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app, path: "/api" });
 
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.send("Hello");
   // res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
